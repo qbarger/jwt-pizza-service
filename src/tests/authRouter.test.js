7d81d973ad26a7a7c87904422549bc088cb8ab62
Quenton Barger <qbarger22@gmail.com>
2025-01-28 15:36:25 -0700

    First auth test

---
+++ src/tests/authRouter.test.js
@@ -0,0 +1,40 @@
+const request = require("supertest");
+const app = require("../service");
+
+const testUser = { name: "pizza diner", email: "reg@test.com", password: "a" };
+let testUserAuthToken;
+
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
+  const registerRes = await request(app).post("/api/auth").send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  expectValidJwt(testUserAuthToken);
+});
+
+test("register", async () => {
+  const user = createUser();
+  const registerRes = await request(app).post("/api/auth").send(user);
+  expect(registerRes.status).toBe(200);
+});
+
+test("login", async () => {
+  const loginRes = await request(app).put("/api/auth").send(testUser);
+  expect(loginRes.status).toBe(200);
+  expectValidJwt(loginRes.body.token);
+
+  const expectedUser = { ...testUser, roles: [{ role: "diner" }] };
+  delete expectedUser.password;
+  expect(loginRes.body.user).toMatchObject(expectedUser);
+});
+
+function expectValidJwt(potentialJwt) {
+  expect(potentialJwt).toMatch(
+    /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/
+  );
+}
+
+function createUser() {
+  const user = { name: "pizza diner", email: "reg@test.com", password: "a" };
+  user.email = Math.random().toString(36).substring(2, 12) + "@test.com";
+  return user;
+}
