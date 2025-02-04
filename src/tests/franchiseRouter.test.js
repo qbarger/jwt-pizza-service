c5c0c13849819d09972f657dc820884138a0c04e
Quenton Barger <qbarger22@gmail.com>
2025-02-04 14:43:58 -0700

    More fixing of tests

--- src/tests/franchiseRouter.test.js
+++ src/tests/franchiseRouter.test.js
@@ -5,114 +5,109 @@ if (process.env.VSCODE_INSPECTOR_OPTIONS) {
   jest.setTimeout(60 * 1000 * 5); // 5 minutes
 }
 
-describe("franchiseRouter tests", () => {
-  const testUser = {
-    name: "pizza diner",
-    email: "reg@test.com",
-    password: "a",
-  };
-  let testUserAuthToken;
+const testUser = {
+  name: "pizza diner",
+  email: "reg@test.com",
+  password: "a",
+};
+let testUserAuthToken;
 
-  const adminUser = {
-    id: 1,
-    name: "常用名字",
-    email: "a@jwt.com",
-    password: "admin",
-    roles: [{ role: "admin" }],
-  };
-  let adminUserAuthToken;
+const adminUser = {
+  id: 1,
+  name: "常用名字",
+  email: "a@jwt.com",
+  password: "admin",
+  roles: [{ role: "admin" }],
+};
+let adminUserAuthToken;
 
-  beforeAll(async () => {
-    testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
-    const registerRes = await request(app).post("/api/auth").send(testUser);
-    testUserAuthToken = registerRes.body.token;
-    expect(typeof testUserAuthToken).toBe("string");
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
+  const registerRes = await request(app).post("/api/auth").send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  expect(typeof testUserAuthToken).toBe("string");
 
-    const loginRes = await request(app).put("/api/auth").send(adminUser);
-    if (loginRes.status !== 200) {
-      registerAdmin();
-    } else {
-      adminUserAuthToken = loginRes.body.token;
-    }
-  });
-
-  test("list franchises", async () => {
-    const res = await request(app).get("/api/franchise");
-    expect(res.status).toBe(200);
-  });
-
-  test("create franchise no admin", async () => {
-    const loginRes = await request(app).put("/api/auth").send(testUser);
-    const token = loginRes.body.token;
-    const res = await request(app)
-      .post("/api/franchise")
-      .set("Authorization", `Bearer ${token}`)
-      .send({ name: "pizzaPocket", admins: [{ email: testUser.email }] });
-    expect(res.status).toBe(403);
-  });
-
-  test("create franchise", async () => {
-    const random = getRandom();
-    const res = await request(app)
-      .post("/api/franchise")
-      .set("Authorization", `Bearer ${adminUserAuthToken}`)
-      .send({
-        name: `pizzaPocket${random}`,
-        admins: [{ email: adminUser.email }],
-      });
-    expect(res.status).toBe(200);
-  });
-
-  test("create store", async () => {
-    const random = getRandom();
-    const res = await request(app)
-      .post("/api/franchise")
-      .set("Authorization", `Bearer ${adminUserAuthToken}`)
-      .send({
-        name: `pizzaPocket${random}`,
-        admins: [{ email: adminUser.email }],
-      });
-    expect(res.status).toBe(200);
-
-    const random2 = getRandom();
-    const franchiseId = res.body.id;
-    const storeRes = await request(app)
-      .post(`/api/franchise/${franchiseId}/store`)
-      .set("Authorization", `Bearer ${adminUserAuthToken}`)
-      .send({ name: `pizzaStore${random2}` });
-    expect(storeRes.status).toBe(200);
-  });
-
-  test("delete franchise", async () => {
-    const random = getRandom();
-    const createRes = await request(app)
-      .post("/api/franchise")
-      .set("Authorization", `Bearer ${adminUserAuthToken}`)
-      .send({
-        name: `pizzaPocket${random}`,
-        admins: [{ email: adminUser.email }],
-      });
-    expect(createRes.status).toBe(200);
-
-    const deleteRes = await request(app)
-      .delete(`/api/franchise/${adminUser.id}`)
-      .set("Authorization", `Bearer ${adminUserAuthToken}`);
-    expect(deleteRes.status).toBe(200);
-  });
-
-  test("list user's franchises", async () => {
-    const res = await request(app)
-      .get(`/api/franchise/${adminUser.id}`)
-      .set("Authorization", `Bearer ${adminUserAuthToken}`);
-    expect(res.status).toBe(200);
-  });
-
-  function getRandom() {
-    return Math.random().toString(36).substring(2, 12);
-  }
-
-  async function registerAdmin() {
-    const registerRes = await request(app).post("/api/auth").send(adminUser);
-    adminUserAuthToken = registerRes.body.token;
-  }
+  const loginRes = await request(app).put("/api/auth").send(adminUser);
+  adminUserAuthToken = loginRes.body.token;
 });
+
+test("list franchises", async () => {
+  const res = await request(app).get("/api/franchise");
+  expect(res.status).toBe(200);
+});
+
+test("create franchise no admin", async () => {
+  const loginRes = await request(app).put("/api/auth").send(testUser);
+  const token = loginRes.body.token;
+  const res = await request(app)
+    .post("/api/franchise")
+    .set("Authorization", `Bearer ${token}`)
+    .send({ name: "pizzaPocket", admins: [{ email: testUser.email }] });
+  expect(res.status).toBe(403);
+});
+
+test("create franchise", async () => {
+  const random = getRandom();
+  const res = await request(app)
+    .post("/api/franchise")
+    .set("Authorization", `Bearer ${adminUserAuthToken}`)
+    .send({
+      name: `pizzaPocket${random}`,
+      admins: [{ email: adminUser.email }],
+    });
+  expect(res.status).toBe(200);
+});
+
+test("create store", async () => {
+  const random = getRandom();
+  const res = await request(app)
+    .post("/api/franchise")
+    .set("Authorization", `Bearer ${adminUserAuthToken}`)
+    .send({
+      name: `pizzaPocket${random}`,
+      admins: [{ email: adminUser.email }],
+    });
+  expect(res.status).toBe(200);
+
+  const random2 = getRandom();
+  const franchiseId = res.body.id;
+  const storeRes = await request(app)
+    .post(`/api/franchise/${franchiseId}/store`)
+    .set("Authorization", `Bearer ${adminUserAuthToken}`)
+    .send({ name: `pizzaStore${random2}` });
+  expect(storeRes.status).toBe(200);
+});
+
+test("delete franchise", async () => {
+  const random = getRandom();
+  const createRes = await request(app)
+    .post("/api/franchise")
+    .set("Authorization", `Bearer ${adminUserAuthToken}`)
+    .send({
+      name: `pizzaPocket${random}`,
+      admins: [{ email: adminUser.email }],
+    });
+  expect(createRes.status).toBe(200);
+
+  const deleteRes = await request(app)
+    .delete(`/api/franchise/${adminUser.id}`)
+    .set("Authorization", `Bearer ${adminUserAuthToken}`);
+  expect(deleteRes.status).toBe(200);
+});
+
+test("list user's franchises", async () => {
+  const res = await request(app)
+    .get(`/api/franchise/${adminUser.id}`)
+    .set("Authorization", `Bearer ${adminUserAuthToken}`);
+  expect(res.status).toBe(200);
+});
+
+function getRandom() {
+  return Math.random().toString(36).substring(2, 12);
+}
+
+async function registerAdmin() {
+  adminUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
+  const registerRes = await request(app).post("/api/auth").send(adminUser);
+  adminUserAuthToken = registerRes.body.token;
+}
--- src/tests/orderRouter.test.js
+++ src/tests/orderRouter.test.js
@@ -5,78 +5,94 @@ if (process.env.VSCODE_INSPECTOR_OPTIONS) {
   jest.setTimeout(60 * 1000 * 5); // 5 minutes
 }
 
-describe("orderRouter tests", () => {
-  const adminUser = {
-    name: "常用名字",
-    email: "a@jwt.com",
-    password: "admin",
-    roles: [{ role: "admin" }],
-  };
-  let adminUserAuthToken;
+const adminUser = {
+  name: "常用名字",
+  email: "a@jwt.com",
+  password: "admin",
+  roles: [{ role: "admin" }],
+};
+let adminUserAuthToken;
 
-  beforeAll(async () => {
-    const loginRes = await request(app).put("/api/auth").send(adminUser);
-    if (loginRes.status !== 200) {
-      registerAdmin();
-    } else {
-      adminUserAuthToken = loginRes.body.token;
-    }
-  });
-
-  test("list menu", async () => {
-    const listres = await request(app).get("/api/order/menu");
-    expect(listres.status).toBe(200);
-  });
-
-  test("add menu item", async () => {
-    const random = getRandom();
-    const res = await request(app)
-      .post("/api/franchise")
-      .set("Authorization", `Bearer ${adminUserAuthToken}`)
-      .send({
-        name: `pizzaPocket${random}`,
-        admins: [{ email: adminUser.email }],
-      });
-    expect(res.status).toBe(200);
-
-    const random2 = getRandom();
-    const franchiseId = res.body.id;
-    const storeRes = await request(app)
-      .post(`/api/franchise/${franchiseId}/store`)
-      .set("Authorization", `Bearer ${adminUserAuthToken}`)
-      .send({ name: `pizzaStore${random2}` });
-    expect(storeRes.status).toBe(200);
-
-    const res2 = await request(app)
-      .put("/api/order/menu")
-      .set("Authorization", `Bearer ${adminUserAuthToken}`)
-      .send({
-        title: "Student",
-        description: "No topping, no sauce, just carbs",
-        image: "pizza9.png",
-        price: 0.0001,
-      });
-    expect(res2.status).toBe(200);
-  });
-
-  test("create order", async () => {
-    const res = await request(app)
-      .post("/api/order")
-      .set("Authorization", `Bearer ${adminUserAuthToken}`)
-      .send({
-        franchiseId: 3,
-        storeId: 8,
-        items: [{ menuId: 1, description: "Veggie", price: 0.05 }],
-      });
-    expect(res.status).toBe(200);
-  });
-
-  function getRandom() {
-    return Math.random().toString(36).substring(2, 12);
-  }
-
-  async function registerAdmin() {
-    const registerRes = await request(app).post("/api/auth").send(adminUser);
-    adminUserAuthToken = registerRes.body.token;
+beforeAll(async () => {
+  const loginRes = await request(app).put("/api/auth").send(adminUser);
+  if (loginRes.status !== 200) {
+    registerAdmin();
+  } else {
+    adminUserAuthToken = loginRes.body.token;
   }
 });
+
+test("list menu", async () => {
+  const listres = await request(app).get("/api/order/menu");
+  expect(listres.status).toBe(200);
+});
+
+test("add menu item", async () => {
+  const random = getRandom();
+  const res = await request(app)
+    .post("/api/franchise")
+    .set("Authorization", `Bearer ${adminUserAuthToken}`)
+    .send({
+      name: `pizzaPocket${random}`,
+      admins: [{ email: adminUser.email }],
+    });
+  expect(res.status).toBe(200);
+
+  const random2 = getRandom();
+  const franchiseId = res.body.id;
+  const storeRes = await request(app)
+    .post(`/api/franchise/${franchiseId}/store`)
+    .set("Authorization", `Bearer ${adminUserAuthToken}`)
+    .send({ name: `pizzaStore${random2}` });
+  expect(storeRes.status).toBe(200);
+
+  const res2 = await request(app)
+    .put("/api/order/menu")
+    .set("Authorization", `Bearer ${adminUserAuthToken}`)
+    .send({
+      title: "Student",
+      description: "No topping, no sauce, just carbs",
+      image: "pizza9.png",
+      price: 0.0001,
+    });
+  expect(res2.status).toBe(200);
+});
+
+test("create order", async () => {
+  const random = getRandom();
+  const res1 = await request(app)
+    .post("/api/franchise")
+    .set("Authorization", `Bearer ${adminUserAuthToken}`)
+    .send({
+      name: `pizzaPocket${random}`,
+      admins: [{ email: adminUser.email }],
+    });
+  expect(res1.status).toBe(200);
+
+  const random2 = getRandom();
+  const franchiseId = res1.body.id;
+  const storeRes = await request(app)
+    .post(`/api/franchise/${franchiseId}/store`)
+    .set("Authorization", `Bearer ${adminUserAuthToken}`)
+    .send({ name: `pizzaStore${random2}` });
+  expect(storeRes.status).toBe(200);
+
+  const res3 = await request(app)
+    .post("/api/order")
+    .set("Authorization", `Bearer ${adminUserAuthToken}`)
+    .send({
+      franchiseId: franchiseId,
+      storeId: storeRes.body.id,
+      items: [{ menuId: 1, description: "Veggie", price: 0.05 }],
+    });
+  expect(res3.status).toBe(200);
+});
+
+function getRandom() {
+  return Math.random().toString(36).substring(2, 12);
+}
+
+async function registerAdmin() {
+  const registerRes = await request(app).post("/api/auth").send(adminUser);
+  adminUserAuthToken = registerRes.body.token;
+}
