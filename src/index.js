0399bbd7af680b37424dd96dd6d95fbc53873496
Quenton Barger <qbarger22@gmail.com>
2025-04-06 13:18:50 -0600

    fixing testing

--- src/index.js
+++ src/index.js
@@ -15,6 +15,10 @@ process.on("unhandledRejection", (reason) => {
   });
 });
 
+if (process.env.NODE_ENV !== "test") {
+  app.use(metrics.track);
+}
+
 const port = process.argv[2] || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
